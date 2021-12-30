65c29dd9f83095ca454f3dc8ac9f88482a8b0ae2
NguyenHuuNhan <nguyenhuunhan4692@gmail.com>
2021-12-30 10:35:55 +0700

    Navigation done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import LeftSideBar from "./components/LeftSideBar";
+import Navigation from "./components/Navigation";
+import Content from "./components/Content";
+import RightSideBar from "./components/RightSideBar";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <main className="main-container">
+        <LeftSideBar />
+        <Content />
+        <RightSideBar />
+      </main>
+    </>
   );
 }
 
